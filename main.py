88fb6f01b914f4a702d92dc80bcb19cf6e36e55d
Scalynko <66178425+Scalynko@users.noreply.github.com>
2026-01-08 13:48:55 -0800

    Fixed Tamagotchis turning gray instead of turning white

--- main.py
+++ main.py
@@ -25,15 +25,29 @@ baseColor = tamagotchi[chosenTama]["baseColor"]
 eyePosition = tamagotchi[chosenTama]["eyePosition"]
 adjustments = tamagotchi[eyeType]["adjustments"]
 
+whiteCount = [0,0,0,0,0,0]
+maxwhite = maxwhiteindex = 0
 
+                    
 if (baseColor != colorPrompt):
     for i in range(image.size[0]):
         for j in range(image.size[1]):
             for x in range(6):
-                # if pixel contains rgb value in the tamagotchi's base color palette, change it to the same indexed one in the desired palette
+            # if pixel contains rgb value in the tamagotchi's base color palette, change it to the same indexed one in the desired palette
                 if pixels[i,j] == color[baseColor][x]:
                     pixels[i,j] = color[colorPrompt][x]
+                    whiteCount[x] += 1
+                    maxwhite = max(whiteCount)
+                    maxwhiteindex = whiteCount.index(maxwhite)
 
+# This is the ugliest way to fix certain tamagotchis turning gray when turning white, I need to learn more about palettes lol
+    if (colorPrompt == "white"):
+        for i in range(image.size[0]):
+                for j in range(image.size[1]):
+                    if pixels[i,j] == color[colorPrompt][maxwhiteindex]:
+                         pixels[i,j] = color[colorPrompt][1]
+                    elif pixels[i,j] == color[colorPrompt][maxwhiteindex+1]:
+                         pixels[i,j] = color[colorPrompt][2]
 
 # The eyes that have blush effects or eyebrows look weird
 eyePosition = (eyePosition[0],eyePosition[1]+adjustments)
@@ -43,8 +57,9 @@ eyePosition = (eyePosition[0],eyePosition[1]+adjustments)
 if os.path.exists("mask/" + chosenTama + ".png"):
     # Making a mask using a new background and a crop of the mask
     mask = Image.open("mask/" + chosenTama + ".png")
-    eyePositionBox = [eyePosition[0], eyePosition[1], eyePosition[0]+40, eyePosition[1]+19]
-    eyeImage = Image.composite(Image.new("RGBA", (40,19)), eyeImage, mask.crop(eyePositionBox))
+    eyePositionBox = [eyePosition[0], eyePosition[1], eyePosition[0]+40, eyePosition[1]+20]
+    eyeImage = Image.composite(Image.new("RGBA", (40,20)), eyeImage, mask.crop(eyePositionBox))
     
 image.paste(eyeImage, eyePosition, eyeImage)
 st.image(image,"",160)
+st.image(image)
